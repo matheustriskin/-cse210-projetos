19dbc22843bd736cdd67c357ddc8cd252ce82d95
matheustriskin <matheus.smarciel@gmail.com>
2026-09-04 14:26:12 -0300

    Conclui Semana 01 - Exercicio 2: Condicionais e Variaveis

--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        Console.Write("Qual é a porcentagem da sua nota? ");
+        string entrada = Console.ReadLine();
+        int porcentagem = int.Parse(entrada);
+
+        string letra = "";
+
+        if (porcentagem >= 90)
+        {
+            letra = "A";
+        }
+        else if (porcentagem >= 80)
+        {
+            letra = "B";
+        }
+        else if (porcentagem >= 70)
+        {
+            letra = "C";
+        }
+        else if (porcentagem >= 60)
+        {
+            letra = "D";
+        }
+        else
+        {
+            letra = "F";
+        }
+
+        Console.WriteLine($"Sua nota conceitual é: {letra}");
+
+        if (porcentagem >= 70)
+        {
+            Console.WriteLine("Parabéns! Você foi aprovado no curso!");
+        }
+        else
+        {
+            Console.WriteLine("Não desanime! Continue se esforçando para a próxima vez!");
+        }
     }
 }
