2345eee0ce31444738099e7af0971f90b45c86f7
matheustriskin <matheus.smarciel@gmail.com>
2026-09-04 14:31:02 -0300

    Conclui Semana 01 - Exercicio 3: Lacos de Repeticao

--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio3.");
+        // Requisito 3: Gera um número aleatório de 1 a 100
+        Random geradorAleatorio = new Random();
+        int numeroMagico = geradorAleatorio.Next(1, 101);
+
+        int palpite = -1;
+
+        // Requisito 2: Laço que continua até o usuário acertar o número mágico
+        while (palpite != numeroMagico)
+        {
+            Console.Write("Qual é o seu palpite? ");
+            palpite = int.Parse(Console.ReadLine());
+
+            // Requisito 1: Condicionais informando se o número é mais alto ou mais baixo
+            if (palpite < numeroMagico)
+            {
+                Console.WriteLine("Mais alto");
+            }
+            else if (palpite > numeroMagico)
+            {
+                Console.WriteLine("Mais baixo");
+            }
+            else
+            {
+                Console.WriteLine("Você adivinhou!");
+            }
+        }
     }
 }
