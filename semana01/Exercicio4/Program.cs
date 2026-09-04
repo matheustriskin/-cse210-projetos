74a956ec6d72cc65e0fd4fb2bdf69bb68553e745
matheustriskin <matheus.smarciel@gmail.com>
2026-09-04 14:32:39 -0300

    Conclui Semana 01 - Exercicio 4: Listas e Genericos

--- semana01/Exercicio4/Program.cs
+++ semana01/Exercicio4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio4.");
+        List<int> numeros = new List<int>();
+
+        Console.WriteLine("Insira uma lista de números e digite 0 quando terminar.");
+
+        int numeroInserido = -1;
+
+        while (numeroInserido != 0)
+        {
+            Console.Write("Insira o número: ");
+            numeroInserido = int.Parse(Console.ReadLine());
+
+            // Não adiciona o 0 à lista
+            if (numeroInserido != 0)
+            {
+                numeros.Add(numeroInserido);
+            }
+        }
+
+        // Requisito 1: Calcular a soma
+        int soma = 0;
+        foreach (int numero in numeros)
+        {
+            soma += numero;
+        }
+        Console.WriteLine($"A soma é: {soma}");
+
+        // Requisito 2: Calcular a média
+        double media = ((double)soma) / numeros.Count;
+        Console.WriteLine($"A média é: {media}");
+
+        // Requisito 3: Encontrar o maior número
+        int maior = numeros[0];
+        foreach (int numero in numeros)
+        {
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+        }
+        Console.WriteLine($"O maior número é: {maior}");
     }
 }
